3ef61a804d83e6280f216ca778826da94c036c1e
KamrynR <62856359+KamrynR@users.noreply.github.com>
2020-09-09 20:03:58 -0500

    test2

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Create React App + Go API WEBPT18 test</h1>
       <h2>
         Deployed with{' '}
         <a
